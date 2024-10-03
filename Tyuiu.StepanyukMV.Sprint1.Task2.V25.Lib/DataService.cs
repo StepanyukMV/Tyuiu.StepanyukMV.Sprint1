03db398670ffca5eb5bd8758593ba71371113175
StepanyukMV <stepanyukmv@outlook.com>
2024-10-03 23:04:08 +0500

    Task2

--- Tyuiu.StepanyukMV.Sprint1.Task2.V25.Lib/DataService.cs
+++ Tyuiu.StepanyukMV.Sprint1.Task2.V25.Lib/DataService.cs
@@ -3,9 +3,9 @@ using System.Security.Cryptography.X509Certificates;
 using tyuiu.cources.programming.interfaces.Sprint1;
 namespace Tyuiu.StepanyukMV.Sprint1.Task2.V25.Lib
 {
-    public class DataService// : //ISprint1Task2V25
+    public class DataService : ISprint1Task2V25
     {
-        public double ConvertRadsToDegrees(double value)
+        public double ConvertRadsToDegrees(int value)
         {
             
             return Math.Round (value * 180 / Double.Pi, 3);
