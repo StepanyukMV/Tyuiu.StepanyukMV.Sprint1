a5573b9795b6a607921dc69664f6d4ff31965798
StepanyukMV <stepanyukmv@outlook.com>
2024-10-03 21:33:38 +0500

    Task1

---
+++ Tyuiu.StepanyukMV.Sprint1.Task1.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.StepanyukMV.Sprint1.Task1.V12.Lib;
+namespace Tyuiu.StepanyukMV.Sprint1.Task1.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-1, res);
+        }
+    }
+}
---
+++ Tyuiu.StepanyukMV.Sprint1.Task1.V12/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.StepanyukMV.Sprint1.Task1.V12.Lib;
+namespace Tyuiu.StepanyukMV.Sprint1.Task1.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Степанюк М.В. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Степанюк М.В. | РППб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (x + y) / 6 и печатает его на экране.    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значения X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значения Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadLine();
+        }
+    }
+}
